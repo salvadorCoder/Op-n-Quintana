df2a0feac63e5657106d1c988c035dadadb223d9
Salvador Nicolas <salvadorn.1111@gmail.com>
2021-10-22 16:27:37 -0300

    subo nuevo PHP, sigue sin enviar mail

--- enviar.php
+++ enviar.php
@@ -1,28 +1,28 @@
 <?php
 
-$nombre = $_POST ['nombre'];
-$apellido = $_POST ['apellido'];
-$celular = $_POST ['celular'];
-$email = $_POST ['email'];
-$genero_no_binario = $_POST ['genero_no_binario'];
-$genero_femenino = $_POST ['genero_femenino'];
-$genero_masculino = $_POST ['genero_masculino'];
-$cuil = $_POST ['cuil'];
-$fecha = $_POST ['fecha'];
-$puestos = $_POST ['puestos'];
-$cocina = $_POST ['cocina'];
-$ayudante = $_POST ['ayudante'];
-$bacha = $_POST ['bacha'];
-$mozo = $_POST ['mozo'];
-$recepcion = $_POST ['recepcion'];
-$comentarios = $_POST ['comentarios'];
-$adjunto = $_POST ['adjunto'];
-$acepta = $_POST ['acepta'];
+$destinatario = 'salvadorn.1111@gmail.com';
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$celular = $_POST['celular'];
+$email = $_POST['email'];
+$genero_no_binario = $_POST['genero_no_binario'];
+$genero_femenino = $_POST ['genero_femenino'];
+$genero_masculino = $_POST['genero_masculino'];
+$cuil = $_POST['cuil'];
+$fecha = $_POST['fecha'];
+$puestos = $_POST['puestos'];
+$cocina = $_POST['cocina'];
+$ayudante = $_POST['ayudante'];
+$bacha = $_POST['bacha'];
+$mozo = $_POST['mozo'];
+$recepcion = $_POST['recepcion'];
+$comentarios = $_POST['comentarios'];
+$adjunto = $_POST['adjunto'];
+$acepta = $_POST['acepta'];
+
+$header = "Enviado desde Opón Ramen House";
+
 
 $message = "Este mensaje fue enviado por: " . $nombre . $apellido . " \r\n";
 $message .= "Su celular es: " . $celular . " \r\n";
@@ -35,12 +35,10 @@ $message .= "Comentarios: " .$_POST['comentarios'] . " \r\n";
 $message .= "CV adjunto: " . $adjunto . " \r\n";
 $message .= "Acepta términos: " . $acepta . " \r\n";
 
-$message .= "Enviado el: " . date('d/m/Y', time());
+mail($destinatario, utf8_decode($message), $header);
 
-$para = 'salvadorn.1111@gmail.com';
-$asunto = 'Mensaje de Sumate! Opón';
-
-mail($para, $asunto, utf8_decode($message), $header);
+echo "<script>alert('¡Gracias, estaremos en contacto con vos!')</script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000) </script>";
 
 header("Location:index.html");
 ?>
